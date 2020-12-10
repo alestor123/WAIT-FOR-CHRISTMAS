97da3b77883f80429c6c118f5bcb4da92d76770c
alestor123 <alestoraldous@gmail.com>
2020-12-11 00:53:20 +0530

    testing proto code

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -6,18 +6,15 @@ christmasOver = new Date(`Dec 26 ${new Date().getFullYear()} 00:00:00`).getTime(
 const intvl = setInterval(() => {
     // Get todays date and time (ms)
     const now = new Date(`Jan 1 ${new Date().getFullYear()} 00:00:00`).getTime()   
-  
     // Distance from now and the launch date (ms)
-    const distance = christmasDate - now;
-  
+    const distance = christmasDate - now;  
     // Time calculation
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
     const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
     // Display result
     message.innerHTML = `
     <div>${days}<span>Days</span></div> 
@@ -25,7 +22,6 @@ const intvl = setInterval(() => {
     <div>${mins}<span>Minutes</span></div>
     <div>${seconds}<span>Seconds</span></div>
     `;
-  
     if(now>christmasOver){
 christmasDate = new Date(`Dec 24, ${new Date().getFullYear()+1} 24:00:00`).getTime();
 christmasOver = new Date(`Dec 25 ${new Date().getFullYear()+1} 24:00:00`).getTime();
